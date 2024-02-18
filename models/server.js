b3f69a383832f87a7f734b90349e302e801eb4b9
oskcarsv <oskcarqsv@gmail.com>
2024-02-18 12:37:37 -0600

    finished user

--- models/server.js
+++ models/server.js
@@ -7,10 +7,13 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
         this.conectarDB();
 
         this.middlewares();
+
+        this.routes();
     }
 
     async conectarDB() {
@@ -21,6 +24,10 @@ class Server {
         this.app.use(express.static('public'));
         this.app.use(cors());
         this.app.use(express.json());
+    }
+
+    routes(){
+        this.app.use(this.usuariosPath, require('../routes/user.routes'));
     }
 
     listen() {
